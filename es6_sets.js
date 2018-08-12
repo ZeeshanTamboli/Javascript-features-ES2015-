91aad58a69a3cd31c09a8acba15edde9bece583a
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-08-12 12:45:40 +0530

    ES6 sets

---
+++ es6_sets.js
@@ -0,0 +1,41 @@
+//SETS - Store unique values of any type
+
+const set1 = new Set();
+
+//Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({ name: 'John' });
+set1.add(true);
+
+// const set2 = new Set([1, 'string', true, { name: 'John' }]);
+// console.log(set2);
+
+console.log(set1);
+
+//Get count
+console.log(set1.size);
+
+//Check for values
+console.log(set1.has(100));
+console.log(set1.has(50 + 50));
+console.log(set1.has({ name: 'John' })); //'Object' is reference type
+
+//Delete from set
+set1.delete(100);
+
+console.log(set1);
+
+//ITERATE through sets
+//for..of loop
+
+for (let item of set1) {
+  console.log(item);
+}
+
+//forEach loop
+set1.forEach(item => console.log(item));
+
+//CONVERT sets to arrays
+const setArr = Array.from(set1);
+console.log(setArr);
